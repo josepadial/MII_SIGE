a8307fc12e6253f4bf7195bb74b059bdd1ae12dd
Jose Padial <joseapadialmolina@gmail.com>
2023-06-02 20:38:22 +0200

    add csv

--- main.py
+++ main.py
@@ -66,3 +66,29 @@ def log_accuracy(path_to_csv, desc, acc, sep='\t', newline='\n'):
 
         csv.write(f'{desc}{sep}{acc}{newline}')
 
+
+import pandas as pd
+
+with open('./data additional/image_attribute_labels.txt', 'r') as file:
+    lines = file.readlines()
+
+new_lines = []
+for line in lines:
+    line = line.strip().split(' ')
+    if len(line) > 5:
+        new_line = [x for x in line if x != '' and x != '0']
+    else:
+        new_line = line
+    new_lines.append(new_line)
+
+with open('./data additional/image_attribute_labels2.txt', 'w') as file:
+    for line in new_lines:
+        file.write(' '.join(line) + '\n')
+
+account = pd.read_csv("./data additional/image_attribute_labels2.txt", sep=' ')
+print("hola")
+
+account.columns = ['id_imagen', 'id_atributo', 'valor si/no', 'certeza del valor', 'tiempo de anotacion']
+
+# store dataframe into csv file
+account.to_csv("./data additional/image_attribute_labels.csv", index=None)
